2d889f303c6f5f8696718dc98b25c3282db9622a
LACY <lc@happytalk.io>
2021-06-07 10:50:48 +0900

    초기파일

---
+++ menu_picker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  menu_picker
+//
+//  Created by LACY on 2021/06/07.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ menu_picker/menu_pickerApp.swift
@@ -0,0 +1,18 @@
+//
+//  menu_pickerApp.swift
+//  menu_picker
+//
+//  Created by LACY on 2021/06/07.
+//
+//
+
+import SwiftUI
+
+@main
+struct menu_pickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
